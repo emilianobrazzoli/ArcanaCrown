244cf0fe95a60e5db0e98f357e7682a58b99476b
fusoux <fusoux@DESKTOP-HFPC66J.lan>
2018-09-07 07:01:20 +0200

    publishing

--- arcanacrown.js
+++ arcanacrown.js
@@ -1,23 +1,23 @@
 	var Discord = require('discord.io');
-	var logger = require('winston');
-	var auth = require('./auth.json');
+	// var logger = require('winston');
+	var auth = require('./auth.json'); // for local
 	var gestional = require('./gestional.js');
         
 
 
-	logger.level = 'debug';
+	// logger.level = 'debug';
 
 	//Initialize Discord Bot
 	var bot = new Discord.Client({
-	    token: auth.token,
+	    token:  process.env.TOKEN, //for glithc.com
 	    autorun: true
 	});
 
-	bot.on('ready', function(evt) {
-	    logger.info('Connected');
-	    logger.info('Logged in as: ');
-	    logger.info(bot.username + ' - (' + bot.id + ')');
-	});
+	// bot.on('ready', function(evt) {
+	//     logger.info('Connected');
+	//     logger.info('Logged in as: ');
+	//     logger.info(bot.username + ' - (' + bot.id + ')');
+	// });
 
 	bot.on('message', function(user, userID, channelID, message, evt) {
 		
